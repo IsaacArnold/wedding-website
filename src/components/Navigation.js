835d016a4e260b1e0f8e9ed7fa7f83b425b9e1ea
Isaac Arnold <isaacarnold@icloud.com>
2022-04-28 19:35:56 +1000

    Nothing

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby";
 import styled from "styled-components";
 import { FiMenu } from "react-icons/fi";
 import Divider from "./Divider";
-import { useLocation } from "@reach/router";
+// import { useLocation } from "@reach/router";
 
 //#region Page styles
 const NavStyles = styled.nav`
@@ -113,13 +113,12 @@ const LaptopLink = styled(Link)`
 let urlArray = [];
 
 const Navigation = () => {
-  const history = useLocation();
-  const pathname = history.pathname;
-  console.log(history);
+  // const history = useLocation();
+  // const pathname = history.pathname;
 
-  useEffect(() => {
-    urlArray.push(pathname);
-  }, []);
+  // useEffect(() => {
+  //   urlArray.push(pathname);
+  // }, []);
 
   const handleClick = () => {
     // Toggle the hidden class
@@ -137,7 +136,7 @@ const Navigation = () => {
       </HamburgerContainer>
       <MobileNavContainer id="dropdown" className="hidden">
         <div className="inner-mobile-nav">
-          <MobileLink to={urlArray[0]}>Your Invitation</MobileLink>
+          <MobileLink to="/">Your Invitation</MobileLink>
           <MobileLink to="/rsvp">RSVP</MobileLink>
           <MobileLink to="/venue">Venue</MobileLink>
           <MobileLink to="/accommodation">Accommodation</MobileLink>
@@ -148,7 +147,7 @@ const Navigation = () => {
       <LaptopNavContainer>
         <div className="inner-laptop-nav">
           <Divider width="60px" />
-          <LaptopLink to={urlArray[0]}>Your Invitation</LaptopLink>
+          <LaptopLink to="/">Your Invitation</LaptopLink>
           <LaptopLink to="/rsvp">RSVP</LaptopLink>
           <LaptopLink to="/venue">Venue</LaptopLink>
           <LaptopLink to="/accommodation">Accommodation</LaptopLink>
