59fa11da484ae980e943af7f5f69a48e8b5fa788
Isaac Arnold <isaacarnold@icloud.com>
2022-04-29 08:24:20 +1000

    Adding Wedding Party page

---
+++ src/pages/wedding-party.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+import Layout from "../components/Layout";
+
+const WeddingParty = () => {
+  return (
+    <Layout>
+      <div>Wedding Party content</div>
+    </Layout>
+  );
+};
+
+export default WeddingParty;
